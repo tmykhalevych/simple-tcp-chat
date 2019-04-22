bcbda9be0cf612e9fef077c4e8f6c0f3ddcd2441
Taras <taras.mykhalevych@gmail.com>
2019-04-22 14:39:14 +0300

    Implement client methods

--- client/source/client.cpp
+++ client/source/client.cpp
@@ -1,5 +1,6 @@
 #include "client.h"
 #include "logger.h"
+#include "message_helpers.inl"
 
 namespace chat {
     client::client(std::string host, std::string port)
@@ -14,7 +15,11 @@ namespace chat {
 
     void client::send(Message& msg) {
         LOG_SCOPE
-        // TODO
+        bool not_writing = _message_q.empty();
+        _message_q.push_back(msg);
+        if (not_writing) {
+            write();
+        }
     }
 
     bool client::join_room(std::string nickname, std::string pass) {
@@ -25,7 +30,15 @@ namespace chat {
 
     void client::connect() {
         LOG_SCOPE
-        // TODO
+        boost::asio::async_connect(
+            _socket,
+            _endpoint,
+            [this](boost::system::error_code ec, tcp::resolver::iterator) {
+                if (!ec) {
+                    read_header_and(std::bind(&client::read, this));
+                }
+            }
+        );
     }
 
     void client::read_header_and(std::function<void(void)> next_action) {
@@ -45,12 +58,48 @@ namespace chat {
 
     void client::read() {
         LOG_SCOPE
-        // TODO
+        boost::asio::async_read(
+            _socket,
+            boost::asio::buffer(_msg_buff, _msg_buff_size),
+            [this](boost::system::error_code ec, std::size_t) {
+                if (!ec) {
+                    LOG_MSG("<--- Receive <message>")
+                    LOG_MSG("message = " + std::string(_msg_buff, _msg_buff_size))
+                    Message msg;
+                    std::istringstream iss(std::string(_msg_buff, _msg_buff_size));
+                    msg.ParseFromIstream(&iss);
+                    _read_callback(msg);
+                }
+                else if (ec != boost::asio::error::operation_aborted) {
+                    end();
+                }
+            }
+        );
     }
 
     void client::write() {
         LOG_SCOPE
-        // TODO
+        boost::asio::streambuf output_buff;
+        std::ostream os(&output_buff);
+        message::add_header(&os, _message_q.front());
+        _message_q.front().SerializeToOstream(&os);
+        boost::asio::async_write(
+            _socket,
+            output_buff,
+            [this](boost::system::error_code ec, std::size_t) {
+                if (!ec) {
+                    LOG_MSG("---> Send message")
+                    // TODO: Add message containings log
+                    _message_q.pop_front();
+                    if (!_message_q.empty()) {
+                        write();
+                    }
+                }
+                else if (ec != boost::asio::error::operation_aborted) {
+                    end();
+                }
+            }
+        );
     }
 
     void client::process_header() {
